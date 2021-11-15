fea02f09e1ec61a4cd7d0283ba8495b0366aa18b
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-15 13:58:28 -0500

    The problem was with single-character cases, not empty strings, and has been fixed. Current code times out on the LeetCode submission checker.

--- longest-substring-without-repeating-characters/src/main/Solution.java
+++ longest-substring-without-repeating-characters/src/main/Solution.java
@@ -5,9 +5,9 @@ public class Solution {
         int longest = 0;
         
         int length = s.length();
-        for (int i = 0; i < length - 1; i++) {
-        	for (int j = i + 1; j < length; j++) {
-        		String sub = s.substring(i, j);
+        for (int i = 0; i < length; i++) {
+        	for (int j = i; j < length; j++) {
+        		String sub = s.substring(i, j + 1);
         		if (sub.length() > longest && hasNoRepeating(sub)) {
         			longest = sub.length();
         		}
@@ -17,11 +17,11 @@ public class Solution {
 		return longest;
     }
 
-	private static boolean hasNoRepeating(String sub) {
+	private static boolean hasNoRepeating(String sub) {		
 		String[] chars = sub.split("");
 		
 		int length = chars.length;
-		for (int i = 0; i < length - 1; i++) {
+		for (int i = 0; i < length; i++) {
 			for (int j = i + 1; j < length; j++) {
 				if (chars[i].equals(chars[j])) {
 					return false;

27075811dff32a799f15f94b1eeef7912a9265ef
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-16 13:00:41 -0500

    First attempt works, but timed out on submission.

--- longest-palindromic-substring/src/Solution.java
+++ longest-palindromic-substring/src/Solution.java
@@ -1,8 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
+    	if (s == null || s.length() < 1) return "";
+    	
+        String palindrome = "";
+        int longest = 0;
         
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+        	// TODO Check for palindromes
+        }
+        
+//        for (int i = 0; i < length; i++) {
+//        	for (int j = i + 1; j <= length; j++) {
+//        		String sub = s.substring(i, j);
+//        		if (sub.length() > longest && isPalindrome(sub)) {
+//        			palindrome = sub;
+//        			longest = palindrome.length();
+//        		}
+//        	}
+//        }
     	
-    	
-    	return null;
+    	return palindrome;
     }
+
+	private boolean isPalindrome(String s) {
+		String[] a = s.split("");
+		
+		int length = a.length;
+		for (int i = 0; i < length / 2; i++) {
+			if (a[i].equals(a[length - i - 1]) == false) return false;
+		}
+		
+		return true;
+	}
 }

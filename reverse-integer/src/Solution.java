6aebf7b144fcfa69e12dfdad055b025790bd4075
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-16 12:13:29 -0500

    Completed the challenge "Reverse Integer"

--- reverse-integer/src/Solution.java
+++ reverse-integer/src/Solution.java
@@ -1,8 +1,27 @@
 
 class Solution {
 	public int reverse(int x) {
+		int y = 0;
+		String str = "";
+			
+		if (x < 0) {
+			x = x * -1;
+			str = "-";
+		}
 		
+		String temp = Integer.toString(x);
+		String[] a = temp.split("");
 		
-		return 0;
+		for (int i = a.length - 1; i >= 0; i--) {
+			str = str.concat(a[i]);
+		}
+		
+		try {
+			y = Integer.valueOf(str);
+		} catch (Exception e) {
+			return 0;
+		}
+		
+		return y;
 	}
 }

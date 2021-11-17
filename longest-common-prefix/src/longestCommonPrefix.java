f55ef270c34c472f876adc9ed1de2b1ec156de85
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-17 16:07:40 -0500

    Completed the challenge "Longest Common Prefix"

--- longest-common-prefix/src/longestCommonPrefix.java
+++ longest-common-prefix/src/longestCommonPrefix.java
@@ -1,6 +1,26 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // TODO Fill out the function
-    	return null;
+    	if (strs.length == 1) return strs[0];
+        
+        String longest = "";
+    	
+    	int length = 1;
+        boolean going = true;
+    	while (going && length <= strs[0].length()) {
+    		String sub = strs[0].substring(0, length);
+    		
+    		for (int i = 1; i < strs.length; i++) {
+                
+    			if (!strs[i].startsWith(sub)){
+                    going = false;
+                    break;
+                }
+    		}
+    		
+    		if (going) longest = sub;
+    		length ++;
+    	}
+    	
+    	return longest;
     }
 }

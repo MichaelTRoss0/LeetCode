47fbc28cb6bab42ae78bfee98b71e0a35fcd46f7
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-18 13:16:27 -0500

    Completed the challenge "Longest Valid Parentheses"

--- valid-parentheses/src/Solution.java
+++ valid-parentheses/src/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
+    	if (s.length() % 2 == 1) return false;
     	
+    	String placeholder = s;
+    	while (placeholder.contains("()") || placeholder.contains("{}") ||
+    			placeholder.contains("[]")) {
+    		placeholder = placeholder.replaceAll("\\(\\)", "");
+    		placeholder = placeholder.replaceAll("\\{}", "");
+    		placeholder = placeholder.replaceAll("\\[]", "");
+            System.out.println(placeholder);
+    	}
+    	
+    	if (placeholder.isEmpty()) return true; 
     	
         return false;
     }

61dbda71f8470bbe42eb8de664c2206b6ab640c6
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-20 12:17:41 -0500

    Completed the challenge "Container With Most Water"

--- container-with-most-water/src/Solution.java
+++ container-with-most-water/src/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
-        return 0;
+    	int area = 0;
+    	int left = 0;
+    	int right = height.length - 1;
+    	
+    	while (left < right) {
+    		int w = right - left;
+    		int l = Math.min(height[left], height[right]);
+    		area = Math.max(area, l * w);
+    		
+    		if (height[left] < height[right])
+    			left++;
+    		else
+    			right--;
+    	}
+    	
+        return area;
     }
 }

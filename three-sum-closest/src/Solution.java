904c59e7e81a3eee10567bfbcd19cb76c8e37046
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-22 18:26:52 -0500

    Completed the challenge "3Sum Closest".

--- three-sum-closest/src/Solution.java
+++ three-sum-closest/src/Solution.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        return 0;
+        int answer = 0;
+        int closest = Integer.MAX_VALUE;
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < n - 2; i++) {
+        	if (i > 0 && nums[i] == nums[i-1]) continue;
+        	
+        	int j = i + 1;
+        	int k = n - 1;
+        	
+        	while (j < k) {
+        		int sum = nums[i] + nums[j] + nums[k];
+        		if (sum == target) return sum;
+        		
+        		int diff = Math.abs(target - sum);
+        		if (diff < closest) {
+        			answer = sum;
+        			closest = diff;
+        		}
+                
+                if (target - sum > 0)
+        			j++;
+        		else 
+        			k--;
+        		
+        	}
+        }
+        
+    	return answer;
     }
 }

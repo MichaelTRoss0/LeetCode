7fe3dfa9ddf42bd798cd41f8c04c43e2b36dc5dc
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-23 12:48:16 -0500

    Began the challenge "4Sum".

---
+++ four-sum/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> answer = new ArrayList<>();
+        
+        return answer;
+    }
+}

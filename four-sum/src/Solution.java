2aef7f8d034e7163712822d0581d3e793e7468a0
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-25 14:12:53 -0500

    Added many trace prints. Current solution seems to have trouble with not iterating through the whole nums array when its too long.

--- four-sum/src/Solution.java
+++ four-sum/src/Solution.java
@@ -24,23 +24,61 @@ class Solution {
     // n is the length of the nums array
     // k is the number of ints left to test
     // x is the position of the curr array
-    // i is the iterator variable
-    private List<List<Integer>> kSum(int[] nums, int n, int target, int k, int x, int i, int[] curr) {
+    // iterator is the iterator variable
+    private List<List<Integer>> kSum(int[] nums, int n, int target, int k, int x, int iterator, int[] curr) {
     	List<List<Integer>> list = new ArrayList<>();
     	int max = n - k + 1;
+        
+        if (k == 2) {
+        	list.addAll(twoPoint(nums, target, iterator + 1, n - 1, curr));
+            System.out.println("Terminate kSum at x = " + x + ", as k == 2"); // Trace
+            System.out.println(); // Trace
+            
+            System.out.println("curr[" + x + "] = " + curr[x]); // Trace
+            System.out.println("END ITERATION: " + x + "-" + iterator); // Trace
+            System.out.println("===================="); // Trace
+            System.out.println(); // Trace
+            System.out.println(); // Trace
+            
+            return list;
+        }
     	
-    	
-    	for ( ; i < max; i++) {
+        ArrayList<Integer> prev = new ArrayList<>();
+    	for (int i = iterator ; i < max; i++) {
     		curr[x] = nums[i];
-    		if (i > 0 && curr[x] == nums[i - 1]) continue;
+            
+            System.out.println("===================="); // Trace
+            System.out.println("BEGIN ITERATION: " + x + "-" + i); // Trace
+            System.out.println("curr[" + x + "] = " + curr[x]); // Trace
+            System.out.println(); // Trace
+            
+            if (prev.contains(curr[x])) {
+                System.out.println("curr[" + x + "] = " + curr[x] + ", which is in prev: " + prev.toString()); // Trace
+    			System.out.print("Continue at i = " + i); // Trace
+                System.out.println(", at x = " + x); // Trace
+                System.out.println(); // Trace
+    			continue;
+    		}
+            prev.add(curr[x]);
     		
-			if (k > 2)
-				list.addAll(kSum(nums, n, target, k - 1, x + 1, i + 1, curr));
-			else {
-				// TODO Add traces
-				list.addAll(twoPoint(nums, target, i + 1, n - 1, curr));
+    		list.addAll(kSum(nums, n, target, k - 1, x + 1, i + 1, curr));
+    		
+			if (i == max - 1) {
+				System.out.print("x, Iterator, i, max: "); // Trace
+				System.out.print(x + ", "); // Trace
+				System.out.print(iterator + ", "); // Trace
+				System.out.print(i + ", "); // Trace
+				System.out.println(max); // Trace
+                System.out.println("Terminate kSum at x = " + x); // Trace
+				System.out.println(); // Trace
 			}
-				
+            
+            System.out.println("curr[" + x + "] = " + curr[x]); // Trace
+            System.out.println("END ITERATION: " + x + "-" + i); // Trace
+            System.out.println("===================="); // Trace
+            System.out.println(); // Trace
+            System.out.println(); // Trace
+            
 		}
 		return list;
     }
@@ -56,7 +94,6 @@ class Solution {
     		
     		sum = takeSum(curr);
             System.out.println(Arrays.toString(curr) + " = " + sum); // Trace
-            System.out.println(); // Trace
     		if (sum == target) {
     			List<Integer> group = new ArrayList<>();
     			for (int x : curr)
@@ -65,10 +102,10 @@ class Solution {
     			
     			j++;
 				k--;
-				while (j < k && nums[j] == nums[j-1])
-					j++;
-				while (j < k && nums[k] == nums[k+1])
-					k--;
+				 while (j < k && nums[j] == nums[j-1])
+				 	j++;
+				 while (j < k && nums[k] == nums[k+1])
+				 	k--;
 				
     		} else if (sum < target)
     			j++;

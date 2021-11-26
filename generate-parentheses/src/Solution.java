c641e4cddade70c5183f9ca3e99f536ebf34d286
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-26 12:29:24 -0500

    Began the challenge "Generate Parentheses".

---
+++ generate-parentheses/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        
+        return ans;
+    }
+}

0c3ceae8cdb322968f03ac1fe28a7581c317f71d
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-26 23:07:05 -0500

    Completed the challenge "Generate Parentheses".

--- generate-parentheses/src/Solution.java
+++ generate-parentheses/src/Solution.java
@@ -5,6 +5,42 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         
+    	ans = generateAll(ans, 2 * n);
+        
         return ans;
+    }
+    
+    private List<String> generateAll(List<String> base, int l) {
+    	List<String> lis = new ArrayList<>();
+    	
+    	String s = "(";
+    	lis.addAll(addNext(s, l - 1));
+    	
+		return lis;
+	}
+    
+    private List<String> addNext(String s, int l) {
+    	List<String> next = new ArrayList<>();
+    	
+    	if (l == 1) {
+    		s = s + ")";
+    		if (isValid(s))
+    			next.add(s);
+    	} else {
+    		next.addAll(addNext(s + "(", l - 1));
+    		next.addAll(addNext(s + ")", l - 1));
+    	}
+    	
+    	return next;
+    }
+    
+    private boolean isValid(String s) {
+    	String placeholder = s;
+    	while (placeholder.contains("()"))
+    		placeholder = placeholder.replaceAll("\\(\\)", "");
+    	
+    	if (placeholder.isEmpty()) return true; 
+    	
+        return false;
     }
 }

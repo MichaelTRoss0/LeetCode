4fc8163e050a4178212eee56c8fffbf451a58dd2
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-11-27 13:35:45 -0500

    Think I'm nearly done. Just need to add a bit more logic for the next part.

--- remove-nth-node-from-end-of-list/src/Solution.java
+++ remove-nth-node-from-end-of-list/src/Solution.java
@@ -1,9 +1,39 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode ln = new ListNode();
+        ListNode ln = new ListNode(0);
         
-        
+        int sz = findSize(head);
+        int x = sz - n;
+                
+        ListNode curr = ln;
+        curr.next = checkNode(head, 0, x);
+        curr = curr.next;
     	
-    	return ln;
+    	return ln.next;
+    }
+    
+    private int findSize(ListNode node) {
+    	int sz = 0;
+    	
+    	ListNode curr = node;
+    	do {
+    		curr = curr.next;
+    		sz++;
+    	} while (curr.next != null);
+    	
+		return sz;
+	}
+
+	private ListNode checkNode(ListNode ln, int i, int x) {
+		ListNode curr = ln;
+    	if (i == x) {
+    		// TODO Add logic to check for null
+    		curr.next = curr.next.next;
+    	} else 
+    		curr.next = checkNode(curr.next, i+1, x);
+    	
+    	curr = curr.next;
+    	
+    	return ln.next;
     }
 }

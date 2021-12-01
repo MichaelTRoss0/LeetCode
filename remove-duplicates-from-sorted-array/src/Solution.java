295ba74a82c6eb7f82948affadac85e697665400
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-12-01 17:59:51 -0500

    Completed the challenge "Remove Duplicates from Sorted Array".

--- remove-duplicates-from-sorted-array/src/Solution.java
+++ remove-duplicates-from-sorted-array/src/Solution.java
@@ -1,7 +1,23 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int u = 0;
         
+    	int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+        	if (u == 0 || isUnique(nums, u, i)) {
+        		nums[u] = nums[i];
+        		u++;
+        	}
+        }
     	
-    	return 0;
+    	return u;
     }
+
+	private boolean isUnique(int[] nums, int u, int x) {		
+		for (int i = 0; i < u; i++) 
+			if (nums[i] == nums[x]) return false;
+		
+		return true;
+	}
 }

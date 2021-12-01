8a8a2fd134a78ce7dd0bf18a6dd82c1f80e7c0fa
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-12-01 17:30:27 -0500

    Completed the challenge "Swap Nodes in Pairs".

--- swap-nodes-in-pairs/src/Solution.java
+++ swap-nodes-in-pairs/src/Solution.java
@@ -1,7 +1,15 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
+    	if (head == null) return null;
+    	if (head.next == null) return head;
     	
+    	ListNode ln = new ListNode();
+    	ln.next = new ListNode();
     	
-        return null;
+    	ln.val = head.next.val;
+    	ln.next.val = head.val;
+    	ln.next.next = swapPairs(head.next.next);
+    	
+    	return ln;
     }
 }

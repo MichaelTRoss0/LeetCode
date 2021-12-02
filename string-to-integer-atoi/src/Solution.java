1c3657c0bc603c31e2e78b440b539139f2a54f0b
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-12-02 14:30:10 -0500

    Completed the challenge "String to Integer (atoi)".

--- string-to-integer-atoi/src/Solution.java
+++ string-to-integer-atoi/src/Solution.java
@@ -1,7 +1,69 @@
 class Solution {
     public int myAtoi(String s) {
+        if (s.isEmpty()) return 0;
+        
+    	int x = 0;
+    	String str = "";
     	
+    	boolean checkWhitespace = true;
+    	boolean checkSign = true;
     	
-        return 0;
+    	boolean isPositive = true;
+    	char c = ' ';
+    	int i = 0;
+    	int n = s.length();
+    	
+    	while (checkWhitespace && i < n) {
+    		c = s.charAt(i);
+    		
+    		if (c == ' ') {
+    			i++;
+    			continue;
+    		}
+    		
+    		checkWhitespace = false;
+    	}
+        
+        if (i == n) return 0;
+    	
+    	while (checkSign) {
+    		c = s.charAt(i);
+    		
+    		if (c == '-') {
+    			i++;
+				isPositive = false;
+    		} else if (c == '+')
+                i++;
+    		
+    		checkSign = false;
+    	}
+        
+        if (i == n) return 0;
+
+    	while (i < n) {
+    		c = s.charAt(i);
+    		if (Character.isDigit(c))
+				str += Character.toString(c);
+			else
+                break;
+    		
+			i++;
+    	}
+    	
+    	if (str.isEmpty()) return 0;
+    	
+    	try {
+    		x = Integer.valueOf(str);
+    		if (!isPositive)
+    			x = -x;
+    	}
+    	catch (Exception e) {
+    		if (isPositive)
+    			return Integer.MAX_VALUE;
+    		else
+    			return Integer.MIN_VALUE;
+    	}
+    	
+        return x;
     }
 }
